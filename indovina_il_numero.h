5d7c3c35a9a5d189f06e465d377be2e516349e6e
Davide <davyflore1809@gmail.com>
2022-04-24 11:04:06 +0200

    aggiunto coso

--- indovina_il_numero.h
+++ indovina_il_numero.h
@@ -7,4 +7,30 @@
 
 #endif //HANDY_H_INDOVINA_IL_NUMERO_H
 
+#include <stdbool.h>
 
+
+int indovina_il_numero(int size, int players[size]) {
+
+    int winner = 0, number, min = 0, max = 999, i, guess;
+    bool won = false;
+
+    number = rand() % 1000;
+
+    printf("\nHo scelto un numero da 0 a 999, indovinatelo!");
+
+    while(won == false) {
+
+        i = 0;
+        while(i < size && won == false) {
+
+            if(players[1] < 0) {
+                //turno giocatore
+            } else {
+                guess = min + (max - min) / 2;//rivedere
+            }
+        }
+    }
+
+    return winner;
+}
